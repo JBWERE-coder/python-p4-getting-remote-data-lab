f8d4c450768c3ec6a3b835439d2662bf18a319d2
JBWERE-coder <lutherwere@gmail.com>
2023-09-19 19:21:35 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,28 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
 
     def load_json(self):
-        pass
+       response_body = self.get_response_body()
+       if response_body:
+            try:
+                json_data = json.loads(response_body)
+                return json_data
+            except json.JSONDecodeError:
+                return None
+       else:
+            return None
+        
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+    json_data = requester.load_json()
+
+    if json_data:
+        # Now you can work with the loaded JSON data
+        print(json_data)
